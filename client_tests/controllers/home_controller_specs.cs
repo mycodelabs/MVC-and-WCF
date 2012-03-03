5b91d4b3e0a95e8763f5300c2023bc018f6009c5
mycodelabs <dheeraj.anteru@gmail.com>
2012-03-03 21:29:19 +0000

    Initially didnt commit the specs to controller and now commiting them

--- client_tests/controllers/home_controller_specs.cs
+++ client_tests/controllers/home_controller_specs.cs
@@ -1,4 +1,8 @@
-﻿using controllers.Home;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using controllers.Home;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
 using Presentation.contracts;
@@ -10,12 +14,38 @@ namespace client_tests.controllers
     {
         public abstract class home_controller_concern : Observes<HomeController>
         {
+            protected static IMeetingTasks the_client_tasks;
+        }
+
+        public class when_home_controller_is_asked_to_return_index_view : home_controller_concern
+        {
+            private static ActionResult result;
+
+            private static MeetingsPageViewModel the_page_view_model;
+
             private Establish c = () =>
                                       {
                                           the_client_tasks = depends.on<IMeetingTasks>();
+                                          the_page_view_model = new MeetingsPageViewModel
+                                                                    {
+                                                                        Meetings =
+                                                                            new List<SelectListItem>
+                                                                                {new SelectListItem()}
+                                                                    };
+                                          the_client_tasks.setup(x => x.GetAllMeetings()).Return(the_page_view_model);
                                       };
 
-            private static IMeetingTasks the_client_tasks;
+            private Because b = () => result = sut.Index();
+
+            private It should_return_a_view = () => result.ShouldBeOfType(typeof (ViewResult));
+
+            private It should_reurn_the_index_view =
+                () => ((ViewResult)result).ViewName.ShouldBeEmpty();
+
+            private It should_return_a_view_containing_pageviewmodel =
+                () => result.downcast_to<ViewResult>().Model.ShouldBeOfType(typeof (MeetingsPageViewModel));
         }
     }
+
+    
 }
